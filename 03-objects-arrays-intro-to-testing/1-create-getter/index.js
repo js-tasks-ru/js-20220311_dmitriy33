6bb77c47a0288d992afd5c3ee693a0b1814a2dc6
dmlvr <lvr@dmlvr>
2022-03-26 16:17:27 +0300

    все задачи 3 блока

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const arr = path.split('.');
+
+    return obj => {
+        let result = obj;
+
+        for (const element of arr) {
+            if (result === undefined) break;
+
+            result = result[element];
+        }
+
+        return result;
+    }
 
 }
